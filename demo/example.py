7a5ac7f72ac947c8117870485016eaac5d936a8f
lohanspies <21362987+lohanspies@users.noreply.github.com>
2020-06-10 12:21:29 +0200

    added import of dotenv module and .env file

--- demo/example.py
+++ demo/example.py
@@ -3,8 +3,17 @@ import asyncio
 
 from aries_basic_controller.aries_controller import AriesAgentController
 
+from dotenv import load_dotenv
+load_dotenv()
+
+admin_url = os.getenv('ADMIN_URL')
+webhook_host = os.getenv('WEBHOOK_HOST')
+webhook_port = os.getenv('WEBHOOK_PORT')
+webhook_base = os.getenv('WEBHOOK_BASE')
+
 async def start_agent():
-    aries = AriesAgentController("https://demo1.myid.africa", 443, "/", True, "https://demo1.myid.africa")
+    aries = AriesAgentController(webhook_host, webhook_port, webhook_base, True, admin_url)
+    AriesAgentController()
     connections = await aries.connections_controller.get_connections()
     print(connections)
     for connection in connections:
