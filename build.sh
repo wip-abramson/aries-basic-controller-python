62dfc6365e71daefad9770af74ee7432467daa2c
wip-abramson <wip.abramson@gmail.com>
2020-07-01 10:00:41 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,1 @@
-echo "Building :latest image"
-docker build -t didx-ai/aries-basic-controller:latest .
-
-# TODO: alter this docker tag name to match account in DockerHub
-#echo "pushing image to Registry"
-#docker push dibbsza/oidc-controller-dot-net:latest
+python3 setup.py sdist bdist_wheel
